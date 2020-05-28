1bd739afc529aabebd88bb73dae53a4fc39b379c
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-05-28 17:16:03 -0500

    remove unused statements in controller

--- src/main/java/academy/ennate/controller/EmployeeController.java
+++ src/main/java/academy/ennate/controller/EmployeeController.java
@@ -16,25 +16,21 @@ public class EmployeeController {
 
     @RequestMapping(method = RequestMethod.GET, value="getById")
     public Employee findOne(@RequestParam(name="id") String empId){
-        System.out.println("idddddddd");
         return service.findOne(empId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value="getByEmail")
     public Employee findByEmail(@RequestParam(name="email") String email){
-        System.out.println("emaillllllllll");
         return service.findByEmail(email);
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Employee> findAll(){
-        System.out.println("findallllllll");
         return service.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Employee create(@RequestBody Employee employee){
-        System.out.println("posttttttttt");
         return service.create(employee);
     }
 
