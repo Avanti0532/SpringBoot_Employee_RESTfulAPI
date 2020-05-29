7f43e9729cbc4febdcbd07d5ad09516615d4e6da
Avanti0532 <42561372+Avanti0532@users.noreply.github.com>
2020-05-29 13:35:38 -0500

    add swagger ui for api documentation

--- src/main/java/academy/ennate/controller/EmployeeController.java
+++ src/main/java/academy/ennate/controller/EmployeeController.java
@@ -2,6 +2,10 @@ package academy.ennate.controller;
 
 import academy.ennate.entity.Employee;
 import academy.ennate.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,31 +19,67 @@ public class EmployeeController {
     private EmployeeService service;
 
     @RequestMapping(method = RequestMethod.GET, value="getById")
+    @ApiOperation(value ="Find employees by employee Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public Employee findOne(@RequestParam(name="id") String empId){
         return service.findOne(empId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value="getByEmail")
+    @ApiOperation(value="Find employees by employee email")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public Employee findByEmail(@RequestParam(name="email") String email){
         return service.findByEmail(email);
     }
 
     @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value="Find all employees")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public List<Employee> findAll(){
         return service.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value="Create an employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public Employee create(@RequestBody Employee employee){
         return service.create(employee);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "{id}")
+    @ApiOperation(value="Update an employee for first name and last name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public Employee update(@PathVariable("id") String empId, @RequestBody Employee employee){
         return service.update(empId,employee);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    @ApiOperation(value="Delete an employee by employee Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public void delete(@PathVariable("id") String empId){
         service.delete(empId);
 
